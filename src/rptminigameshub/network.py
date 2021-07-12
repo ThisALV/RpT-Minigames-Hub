34955ab0adea8f44761c7f47e436d3c05095033c
ThisALV <lel.charriere@orange.fr>
2021-07-12 18:25:10 +0200

    ClientsListener ctor requires every server details

--- src/rptminigameshub/network.py
+++ src/rptminigameshub/network.py
@@ -8,11 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 class ClientsListener:
-    """Listens for incoming WSS connections on given port, then uses established connections with StatusEmitter."""
+    """Listens for incoming WSS connections on given port, then waits for clients to send a `REQUEST` message or for source Subject to emit
+    a value for sending the new game servers data to clients."""
 
-    def __init__(self, port: int, security_ctx: ssl.SSLContext, status_source: rptminigameshub.checkout.Subject):
+    def __init__(self, port: int, servers_config: "dict", security_ctx: ssl.SSLContext, status_source: rptminigameshub.checkout.Subject):
         """Configures shortly started server to listen on given port with given security context configuration. New status are obtained
-        from given source Subject."""
+        from given source Subject.
+        Servers configuration argument contains details about each server which are not related to their status or availability. These
+        details will be sent to client with retrieved status data."""
         pass
 
     async def start(self):
